a6cc5927ceba98653ccf8cd4ac9692e4c234f1ab
DQNEO <dqneoo@gmail.com>
2015-03-29 23:29:58 +0900

    it works!

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/src/Application.php';
+use Symfony\Component\HttpFoundation\Response;
 
 $app = new WAF;
 
+$app->get('/', function () {
+    return new Response('hell world');
+});
+
+$app->run();
+
+/*
 echo "hello world\n";
 
 echo "<pre>\n";
 print_r($_SERVER);
+*/
 
